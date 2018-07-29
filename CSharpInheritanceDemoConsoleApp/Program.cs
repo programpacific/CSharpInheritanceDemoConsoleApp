7fa6b51aa7d912dc1718625b4f6d2e3040de3927
programpacific <programpacific@users.noreply.github.com>
2018-07-28 22:14:25 -0600

    Initial commit.

--- CSharpInheritanceDemoConsoleApp/Program.cs
+++ CSharpInheritanceDemoConsoleApp/Program.cs
@@ -10,6 +10,16 @@ namespace CSharpInheritanceDemoConsoleApp
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+
+            // Added just to make sure it's working alright!
+            Console.ReadLine();
+
+
         }
     }
 }
